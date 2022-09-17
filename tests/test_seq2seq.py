20a2bd3cf0267bc01d752a714eacec3b3ec412c9
eunsour <woo569628@naver.com>
2022-09-17 23:42:03 +0900

    Renamed test_t5 to test_seq2seq

--- tests/test_seq2seq.py
+++ tests/test_seq2seq.py
@@ -3,17 +3,36 @@ import pytest
 
 from generations.seq2seq.seq2seq_model import Seq2SeqModel
 
-def test_t5():
+
+@pytest.mark.parametrize(
+    "model_type, model_name",
+    [
+        ("mt5", "google/mt5-base"),
+        ("m2m", "facebook/m2m100_418M"),
+        # ("reformer", "google/reformer-crime-and-punishment"),
+        # ("xlnet", "xlnet-base-cased"),
+        # ("xlm", "xlm-mlm-17-1280"),
+        # ("roberta", "roberta-base"),
+        # ("distilbert", "distilbert-base-uncased"),
+        # ("albert", "albert-base-v1"),
+        # ("camembert", "camembert-base"),
+        # ("xlmroberta", "xlm-roberta-base"),
+        # ("flaubert", "flaubert-base-cased"),
+    ],
+)
+
+
+def test_t5(model_type, model_name):
     train_data = [
-        ["translate English to Korean", "transformer", "트랜스포머"],
-        ["translate English to Korean", "huggingface", "허깅페이스"],
+        ["transliterate English to Korean", "transformer", "트랜스포머"],
+        ["transliterate English to Korean", "huggingface", "허깅페이스"],
     ]
 
     train_df = pd.DataFrame(train_data, columns=["prefix", "input_text", "target_text"])
 
     eval_data = [
-        ["translate English to Korean", "three", "쓰리"],
-        ["translate English to Korean", "four", "포"],
+        ["transliterate English to Korean", "three", "쓰리"],
+        ["transliterate English to Korean", "four", "포"],
     ]
 
     eval_df = pd.DataFrame(eval_data, columns=["prefix", "input_text", "target_text"])
@@ -21,6 +40,7 @@ def test_t5():
     eval_df = train_df.copy()
 
     model_args = {
+        "output_dir":'./outputs',
         "reprocess_input_data": True,
         "overwrite_output_dir": True,
         "max_seq_length": 10,
@@ -33,7 +53,7 @@ def test_t5():
     }
 
     # Create T5 Model
-    model = Seq2SeqModel("t5", "t5-base", args=model_args)
+    model = Seq2SeqModel(model_type, model_name, args=model_args)
 
     # Train T5 Model on new task
     model.train_model(train_df)
@@ -55,4 +75,3 @@ def test_t5():
 
 if __name__ == "__main__":
     test_t5()
-    
