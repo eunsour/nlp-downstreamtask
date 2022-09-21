25b6aa9089845ae66926f2892b7d822b5d572d24
eunsour <woo569628@naver.com>
2022-09-21 16:45:08 +0900

    modified Seq2Seq test

--- tests/test_seq2seq.py
+++ tests/test_seq2seq.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+from generations.config.model_args import Seq2SeqTrainingArguments
 
 from generations.seq2seq import Seq2SeqModel
 
@@ -20,7 +21,8 @@ from generations.seq2seq import Seq2SeqModel
         # ("flaubert", "flaubert-base-cased"),
     ],
 )
-def test_t5(model_type, model_name):
+
+def test_seq2seq(model_type, model_name):
     train_data = [
         ["transliterate English to Korean", "transformer", "트랜스포머"],
         ["transliterate English to Korean", "huggingface", "허깅페이스"],
@@ -29,43 +31,41 @@ def test_t5(model_type, model_name):
     train_df = pd.DataFrame(train_data, columns=["prefix", "input_text", "target_text"])
 
     eval_data = [
-        ["transliterate English to Korean", "three", "쓰리"],
-        ["transliterate English to Korean", "four", "포"],
+        ["transliterate English to Korean", "Factory", "팩토리"],
+        ["transliterate English to Korean", "Pytorch", "파이토치"],
     ]
 
     eval_df = pd.DataFrame(eval_data, columns=["prefix", "input_text", "target_text"])
 
-    eval_df = train_df.copy()
-
-    model_args = {
-        "output_dir": "./outputs",
-        "reprocess_input_data": True,
-        "overwrite_output_dir": True,
-        "max_seq_length": 10,
-        "train_batch_size": 2,
-        "num_train_epochs": 2,
-        "save_model_every_epoch": False,
-        "max_length": 20,
-        "num_beams": 1,
-    }
-
-    # Create T5 Model
+    model_args = Seq2SeqTrainingArguments(
+        output_dir="./outputs",
+        generation_num_beams=1,
+        generation_max_length=24,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        weight_decay=0.01,
+        num_train_epochs=3,
+        no_cuda=False,
+        auto_find_batch_size=True,
+    )
+    
+    # Create Seq2Seq Model
     model = Seq2SeqModel(model_type, model_name, args=model_args)
 
-    # Train T5 Model on new task
+    # Train Seq2Seq Model on new task
     model.train_model(train_df)
 
-    # Evaluate T5 Model on new task
+    # Evaluate Seq2Seq Model on new task
     model.eval_model(eval_df)
 
-    # Predict with trained T5 model
-    model.predict(["convert: four", "convert: five"])
+    # Predict with trained Seq2Seq model
+    print(model.predict(["machine learning", "deep learning"]))
 
     # Load test
     model = Seq2SeqModel(model_type, model_name, args=model_args)
 
-    # Evaluate T5 Model on new task
+    # Evaluate Seq2Seq Model on new task
     model.eval_model(eval_df)
 
-    # Predict with trained T5 model
-    model.predict(["convert: four", "convert: five"])
+    # Predict with trained Seq2Seq model
+    print(model.predict(["machine learning", "deep learning"]))
