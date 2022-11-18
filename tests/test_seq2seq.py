92a365a44a74e477d65cbd4949e1990b1622f58c
eunsour <woo569628@naver.com>
2022-11-18 21:49:06 +0900

    modified test_seq2seq

--- tests/test_seq2seq.py
+++ tests/test_seq2seq.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import pytest
-from generations.config.model_args import Seq2SeqTrainingArguments
 
-from generations.seq2seq import Seq2SeqModel
+from downstreamtask.seq2seq import Seq2SeqModel
+from downstreamtask.seq2seq.seq2seq_util import split_data
+from downstreamtask.config.model_args import Seq2SeqTrainingArguments
 
 
 @pytest.mark.parametrize(
@@ -10,53 +10,43 @@ from generations.seq2seq import Seq2SeqModel
     [
         ("mt5", "google/mt5-base"),
         ("m2m", "facebook/m2m100_418M"),
-        # ("reformer", "google/reformer-crime-and-punishment"),
-        # ("xlnet", "xlnet-base-cased"),
-        # ("xlm", "xlm-mlm-17-1280"),
-        # ("roberta", "roberta-base"),
-        # ("distilbert", "distilbert-base-uncased"),
-        # ("albert", "albert-base-v1"),
-        # ("camembert", "camembert-base"),
-        # ("xlmroberta", "xlm-roberta-base"),
-        # ("flaubert", "flaubert-base-cased"),
+        ("mbart", "facebook/mbart-large-50"),
     ],
 )
-
 def test_seq2seq(model_type, model_name):
-    train_data = [
-        ["transliterate English to Korean", "transformer", "트랜스포머"],
-        ["transliterate English to Korean", "huggingface", "허깅페이스"],
+    data = [
+        "micwhite\t마이화이트",
+        "hydralight\t하이드라라이트",
+        "characdoor\t케락도어",
+        "acevan\t에이스밴",
+        "worldtec\t월드텍",
+        "unievall\t유니발",
+        "psddr\t피에스디알",
+        "padishah\t파디샤",
+        "unlook\t언룩",
+        "keratherm\t케라섬",
     ]
 
-    train_df = pd.DataFrame(train_data, columns=["prefix", "input_text", "target_text"])
-
-    eval_data = [
-        ["transliterate English to Korean", "Factory", "팩토리"],
-        ["transliterate English to Korean", "Pytorch", "파이토치"],
-    ]
-
-    eval_df = pd.DataFrame(eval_data, columns=["prefix", "input_text", "target_text"])
+    raw = split_data(data, ratio=0.8)
 
     model_args = Seq2SeqTrainingArguments(
-        output_dir="./outputs",
-        generation_num_beams=1,
-        generation_max_length=24,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
-        weight_decay=0.01,
-        num_train_epochs=3,
-        no_cuda=False,
         auto_find_batch_size=True,
+        generation_max_length=24,
+        no_cuda=True,
+        num_train_epochs=1,
+        overwrite_output_dir=True,
+        per_device_eval_batch_size=1,
+        per_device_train_batch_size=1,
     )
-    
+
     # Create Seq2Seq Model
     model = Seq2SeqModel(model_type, model_name, args=model_args)
 
     # Train Seq2Seq Model on new task
-    model.train_model(train_df)
+    model.train_model(raw)
 
     # Evaluate Seq2Seq Model on new task
-    model.eval_model(eval_df)
+    model.eval_model(raw)
 
     # Predict with trained Seq2Seq model
     print(model.predict(["machine learning", "deep learning"]))
@@ -65,7 +55,7 @@ def test_seq2seq(model_type, model_name):
     model = Seq2SeqModel(model_type, model_name, args=model_args)
 
     # Evaluate Seq2Seq Model on new task
-    model.eval_model(eval_df)
+    model.eval_model(raw)
 
     # Predict with trained Seq2Seq model
     print(model.predict(["machine learning", "deep learning"]))
